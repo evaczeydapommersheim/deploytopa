94a95ec5c7f140573ca505e1c6ae73c5665be4c4
Eva Czeyda-Pommersheim <epommersheim@yahoo.co.uk>
2024-01-07 22:42:02 +0000

    fixing dbconfig for pa

---
+++ dbconfig.py
@@ -0,0 +1,6 @@
+mysql = {
+    'host':"EvaCzeydaPommersheim.mysql.pythonanywhere-services.com",
+    'user':"EvaCzeydaPommers",
+    'password':"datareppw2024",
+    'database':"EvaCzeydaPommers$datarepresentation"
+}
